908c54733ba1e064751d9ae60f74694d3a967002
JianGao <jiangao@qiwu.ai>
2018-11-01 09:50:50 +0800

    解压Weather服务初始化Springboot框架文件

---
+++ WeatherServer/src/main/java/com/Weather/WeatherServer/WeatherServerApplication.java
@@ -0,0 +1,12 @@
+package com.Weather.WeatherServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherServerApplication.class, args);
+	}
+}
