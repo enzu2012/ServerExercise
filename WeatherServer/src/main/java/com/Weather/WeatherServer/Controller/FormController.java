f2dd084f81ac2ea07897529266cf1221cd78c627
JianGao <jiangao@qiwu.ai>
2018-11-01 10:44:36 +0800

    修改路径错误

--- WeatherServer/src/main/java/com/Weather/WeatherServer/Controller/FormController.java
+++ WeatherServer/src/main/java/com/Weather/WeatherServer/Controller/FormController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/hello")
 public class FormController {
-    @RequestMapping(value = "sayhello",method = RequestMethod.GET)
+    @RequestMapping(value = "/sayhello",method = RequestMethod.GET)
     public String sayhello(@RequestParam(value = "usm") String usm){
         return "Hello World!\nWelcome "+usm;
     }
