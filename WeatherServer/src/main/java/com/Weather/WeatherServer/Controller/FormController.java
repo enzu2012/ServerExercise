e02e8f2587d0aeca4a06b2e269c606037a473517
JianGao <jiangao@qiwu.ai>
2018-11-02 17:19:53 +0800

    修改controller路径及request方法

--- WeatherServer/src/main/java/com/Weather/WeatherServer/Controller/FormController.java
+++ WeatherServer/src/main/java/com/Weather/WeatherServer/Controller/FormController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/hello")
 public class FormController {
-    @RequestMapping(value = "/sayhello",method = RequestMethod.GET)
-    public String sayhello(@RequestParam(value = "usm") String usm){
-        return "Hello World!\nWelcome "+usm;
+    @RequestMapping(value = "/hello")
+    public String sayhello(){
+        return "Hello World!";
     }
 }
